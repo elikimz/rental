60fbb4a397a4d2314644a85fcc78cecbea6f09bb
Elijah Kimani <elijahkimani1293@gmail.com>
2025-02-26 09:32:32 +0300

    first commit

--- src/features/units/unitsAPI.tsx
+++ src/features/units/unitsAPI.tsx
@@ -14,7 +14,7 @@ interface CreateUnitRequest {
 export const unitAPI = createApi({
   reducerPath: 'unitAPI',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'https://renta-dxdls5gdg2c0a8dhdt.southafricanorth-01.azurewebsites.net',
+    baseUrl: 'https://rentals-dxd5gdg2c0a8dhdt.southafricanorth-01.azurewebsites.net',
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('token');
       if (token) {
@@ -25,14 +25,14 @@ export const unitAPI = createApi({
   }),
   endpoints: (builder) => ({
     getAllUnits: builder.query<Unit[], void>({
-      query: () => '/units', // Fixed the double /units issue
+      query: () => '/units/', // Fixed the double /units issue
     }),
     getUnitById: builder.query<Unit, number>({
-      query: (id) => `/units${id}`,
+      query: (id) => `/units/${id}`,
     }),
     createUnit: builder.mutation<Unit, CreateUnitRequest>({
       query: (newUnit) => ({
-        url: '/units',
+        url: '/units/',
         method: 'POST',
         body: newUnit,
       }),
